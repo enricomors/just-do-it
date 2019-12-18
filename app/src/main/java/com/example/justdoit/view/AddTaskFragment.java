ed5cf914a958c455021baacfb0a7f77c84e7302b
enricomors <enrico.morselli@studio.unibo.it>
2019-12-18 11:58:53 +0100

    added layout for addTaskFragment

--- app/src/main/java/com/example/justdoit/view/AddTaskFragment.java
+++ app/src/main/java/com/example/justdoit/view/AddTaskFragment.java
@@ -12,6 +12,8 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.justdoit.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -27,10 +29,27 @@ public class AddTaskFragment extends Fragment {
     @BindView(R.id.fabDone)
     FloatingActionButton fabDone;
 
+    @BindView(R.id.button_class)
+    Button buttonClass;
+
+    @BindView(R.id.button_date)
+    Button buttonDate;
+
+    @BindView(R.id.button_priority)
+    Button buttonPriority;
+
+    @BindView(R.id.button_time)
+    Button buttonTime;
+
+    @BindView(R.id.edit_text_description)
+    EditText textDescription;
+
+    @BindView(R.id.edit_text_title)
+    EditText textTitle;
+
     public AddTaskFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,10 +64,24 @@ public class AddTaskFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         fabDone.setOnClickListener(v -> onTaskSave());
+        buttonDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // open date picker
+            }
+        });
+        buttonTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // open time picker
+            }
+        });
     }
 
     private void onTaskSave() {
         NavDirections action = AddTaskFragmentDirections.actionSaveTask();
         Navigation.findNavController(fabDone).navigate(action);
+
+        // Save newly created task to database
     }
 }
