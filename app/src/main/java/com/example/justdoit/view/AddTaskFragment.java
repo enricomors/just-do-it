c3508786857418b586c96afc7f1aa3b910c92731
enricomors <enrico.morselli@studio.unibo.it>
2019-12-22 12:30:30 +0100

    added listeners for spinners

--- app/src/main/java/com/example/justdoit/view/AddTaskFragment.java
+++ app/src/main/java/com/example/justdoit/view/AddTaskFragment.java
@@ -21,6 +21,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -30,6 +31,7 @@ import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.example.justdoit.R;
+import com.example.justdoit.model.Task;
 import com.example.justdoit.model.TaskClass;
 import com.example.justdoit.viewmodel.TaskClassViewModel;
 import com.example.justdoit.viewmodel.TaskViewModel;
@@ -73,8 +75,10 @@ public class AddTaskFragment extends Fragment
 
     private TaskViewModel taskViewModel;
     private TaskClassViewModel taskClassViewModel;
-    private ArrayAdapter<String> classAdapter;
+    private ArrayAdapter<TaskClass> classAdapter;
     private ArrayAdapter<CharSequence> priorityAdapter;
+    private TaskClass classSelected;
+    private int prioritySelected;
 
     public AddTaskFragment() {
         // Required empty public constructor
@@ -90,11 +94,11 @@ public class AddTaskFragment extends Fragment
         priorityAdapter = ArrayAdapter.createFromResource(getContext(), R.array.priority_array,
                 android.R.layout.simple_spinner_dropdown_item);
 
-        // populate the adapter for the spinner
+        // populate the adapter for the class spinner w/ data coming from the ViewModel
         taskClassViewModel = ViewModelProviders.of(this).get(TaskClassViewModel.class);
         taskClassViewModel.getAllClasses().observe(this, taskClasses -> {
             for (TaskClass taskClass : taskClasses) {
-                classAdapter.add(taskClass.toString());
+                classAdapter.add(taskClass);
             }
         });
 
@@ -111,6 +115,28 @@ public class AddTaskFragment extends Fragment
         super.onViewCreated(view, savedInstanceState);
 
         fabDone.setOnClickListener(v -> onTaskSave());
+        spinnerClass.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
+                classSelected = (TaskClass) parent.getItemAtPosition(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        spinnerPriority.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int i, long l) {
+                prioritySelected = (int) parent.getItemIdAtPosition(i);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
         buttonTime.setOnClickListener(v -> {
             // open date picker
             DialogFragment timePickerFragment = new TimePickerFragment();
@@ -129,9 +155,18 @@ public class AddTaskFragment extends Fragment
         NavDirections action = AddTaskFragmentDirections.actionSaveTask();
         Navigation.findNavController(fabDone).navigate(action);
 
+        String title = textTitle.getText().toString();
+        String desc = textDescription.getText().toString();
+        String date = buttonDate.getText().toString();
+        String time = buttonTime.getText().toString();
+
+        int classTaskId = classSelected.getClassId();
+        int priority = prioritySelected;
+
+        Task newTask = new Task(title, desc, date, time, priority, classTaskId);
+
         // Save newly created task to database
-        // Task newTask = new Task(...data from fields...)
-        // taskViewModel.insertTask(newTask);
+        taskViewModel.insertTask(newTask);
     }
 
     @Override
